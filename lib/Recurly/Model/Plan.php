c0b9a14814c32283e3bd87926527906b588994ad
Bram Van der Sype <bvandersype@moovly.com>
2014-01-21 13:01:16 +0100

    Added plans as a resource

--- lib/Recurly/Model/Plan.php
+++ lib/Recurly/Model/Plan.php
@@ -8,6 +8,40 @@ class Plan implements ModelInterface
     protected $plan_code;
     /** @var string */
     protected $name;
+    /** @var string */
+    protected $description;
+    /** @var string */
+    protected $success_url;
+    /** @var string */
+    protected $cancel_url;
+    /** @var boolean */
+    protected $display_donation_amounts;
+    /** @var boolean */
+    protected $display_quantity;
+    /** @var boolean */
+    protected $display_phone_number;
+    /** @var boolean */
+    protected $bypass_hosted_confirmation;
+    /** @var string */
+    protected $unit_name;
+    /** @var string */
+    protected $payment_page_tos_link;
+    /** @var integer */
+    protected $plan_interval_length;
+    /** @var string */
+    protected $plan_interval_unit;
+    /** @var integer */
+    protected $trial_interval_length;
+    /** @var string */
+    protected $trial_interval_unit;
+    /** @var string */
+    protected $accounting_code;
+    /** @var \DateTime */
+    protected $created_at;
+    /** @var array */
+    protected $unit_amount_in_cents;
+    /** @var array */
+    protected $setup_fee_in_cents;
 
     /**
      * Returns a mapping
@@ -23,6 +57,57 @@ class Plan implements ModelInterface
                 ],
                 'name' => [
                     'type' => 'string',
+                ],
+                'description' => [
+                    'type' => 'string',
+                ],
+                'success_url' => [
+                    'type' => 'string',
+                ],
+                'cancel_url' => [
+                    'type' => 'string',
+                ],
+                'display_donation_amounts' => [
+                    'type' => 'boolean',
+                ],
+                'display_quantity' => [
+                    'type' => 'boolean',
+                ],
+                'display_phone_number' => [
+                    'type' => 'boolean',
+                ],
+                'bypass_hosted_confirmation' => [
+                    'type' => 'boolean',
+                ],
+                'unit_name' => [
+                    'type' => 'string',
+                ],
+                'payment_page_tos_link' => [
+                    'type' => 'string',
+                ],
+                'plan_interval_length' => [
+                    'type' => 'string',
+                ],
+                'plan_interval_unit' => [
+                    'type' => 'string',
+                ],
+                'trial_interval_length' => [
+                    'type' => 'string',
+                ],
+                'trial_interval_unit' => [
+                    'type' => 'string',
+                ],
+                'accounting_code' => [
+                    'type' => 'string',
+                ],
+                'created_at' => [
+                    'type' => 'datetime',
+                ],
+                'unit_amount_in_cents' => [
+                    'type' => 'assocArray<string>',
+                ],
+                'setup_fee_in_cents' => [
+                    'type' => 'assocArray<string>',
                 ],
             ],
         ];
